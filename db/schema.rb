9ca16052eab5b107a0bf094e374d3d28dc04bf87
laurenfazah <laurenfazah@gmail.com>
2016-04-20 14:28:06 -0400

    fetched origin/solution's comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803185331) do
+ActiveRecord::Schema.define(version: 20150804215003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150803185331) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text   "content"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
