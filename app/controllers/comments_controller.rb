7adc3060a6a27009df14ac907f3f56d7ac140854
racheltstevens <racheltstevens@gmail.com>
2016-11-17 13:33:59 -0500

    Comment out methods on comments controller.
    To drive development.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,34 +2,34 @@ class CommentsController < ApplicationController
   before_filter :set_comment, only: [:show, :update, :destroy]
 
   def index
-    render json: Comment.all
+    # render json: Comment.all
   end
 
   def show
-    render json: @comment
+    # render json: @comment
   end
 
   def create
-    @comment = Comment.new(comment_params)
-
-    if @comment.save
-      render json: @comment, status: :created
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
+    # @comment = Comment.new(comment_params)
+    #
+    # if @comment.save
+    #   render json: @comment, status: :created
+    # else
+    #   render json: @comment.errors, status: :unprocessable_entity
+    # end
   end
 
   def update
-    if @comment.update(comment_params)
-      render json: @comment, status: :ok
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
+    # if @comment.update(comment_params)
+    #   render json: @comment, status: :ok
+    # else
+    #   render json: @comment.errors, status: :unprocessable_entity
+    # end
   end
 
   def destroy
-    @comment.destroy
-    head :no_content
+    # @comment.destroy
+    # head :no_content
   end
 
   private
