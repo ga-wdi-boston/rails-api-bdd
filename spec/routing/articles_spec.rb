606abc17f7f64353c45562b927618eb40a3b6156
racheltstevens <racheltstevens@gmail.com>
2016-11-28 10:25:00 -0500

    Implemented route for get articles
    Unit test passes

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'routes for articles' do
-  skip 'routes GET /articles to the articles#index action' do
+  it 'routes GET /articles to the articles#index action' do
+    # check that a get request to /articles routes to the index method
+    # of the articles controller
+    expect(get('/articles')).to route_to('articles#index')
   end
 
   skip 'routes GET /articles/:id to the articles#show action' do
