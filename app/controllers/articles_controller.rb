9011f3d45ced3d8835155c314147227e877ef6a0
racheltstevens <racheltstevens@gmail.com>
2016-11-28 16:43:05 -0500

    Implement articles controller for post
    * implement controller test
    * feature test passes!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
   end
 
   private
